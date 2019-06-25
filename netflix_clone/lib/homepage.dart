98a4cea20f10b8aa2def27ce0ee1748a87db14d2
Oliver Cripps <ollycripps@gmail.com>
2019-06-25 15:12:16 +0100

    Small refactor

--- netflix_clone/lib/homepage.dart
+++ netflix_clone/lib/homepage.dart
@@ -6,6 +6,7 @@ class HomePage extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(
         primaryColor: Colors.black,
+        brightness: Brightness.dark,
       ), // ThemeData
       home: HomeView(),
     ); // MaterialApp
@@ -14,103 +15,105 @@ class HomePage extends StatelessWidget {
 
 class HomeView extends StatelessWidget {
 
-  TextStyle topMenuStyle = new TextStyle(fontFamily: 'Avenir next', fontSize: 15, color: Colors.white, fontWeight: FontWeight.w600);
+  TextStyle topMenuStyle = new TextStyle(fontFamily: 'Avenir next', fontSize: 20, color: Colors.white, fontWeight: FontWeight.w600);
   TextStyle buttonInfoStyle = new TextStyle(fontFamily: 'Avenir next', fontSize: 10, color: Colors.white, fontWeight: FontWeight.w600);
-  TextStyle buttonBlackStyle = new TextStyle(fontFamily: 'Avenir next', fontSize: 10, color: Colors.black, fontWeight: FontWeight.w600);
+  // TextStyle buttonBlackStyle = new TextStyle(fontFamily: 'Avenir next', fontSize: 10, color: Colors.black, fontWeight: FontWeight.w600);
 
   @override
   Widget build(BuildContext context) {
-    return new Container(
-      // color: Colors.red,
-      child: Center(
-        child: ListView(
-          children: <Widget>[
-            Container(
-              height: 550,
-              decoration: new BoxDecoration(
-                image: new DecorationImage(
-                  image: new AssetImage("lib/assets/starwars.jpg"),
-                  fit: BoxFit.fitWidth
-                ), // DecorationImage
-              ), // BoxDecoration
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Container(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        Container(
-                          height: 50,
-                          width: 50,
-                            child: Image(
-                              image: AssetImage("lib/assets/netflix.png")
-                            ), // Image
-                        ), // Container
-                        FlatButton(
-                          child: Text('Series', style: topMenuStyle),
-                        ), // FlatButton
-                        FlatButton(
-                          child: Text('Films', style: topMenuStyle),
-                        ), // FlatButton
-                        FlatButton(
-                          child: Text('My List', style: topMenuStyle),
-                        ), // FlatButton
-                      ], // Widget[]
-                    ), // Row
-                  ), // Container
-                ], // Widget[]
-              ), // Column
-            ), // Container
-            Container(
-              padding: EdgeInsets.only(top: 20, bottom: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  FlatButton(
-                    child: Column(
-                      children: <Widget>[
-                        Icon(Icons.add, color: Colors.white, size: 30),
-                        Text("My List", style: buttonInfoStyle)
-                      ], // <Widget>[]
-                    ), // Column
-                    onPressed: () {
+    return new Material(
+      child: Container(
+        // color: Colors.red,
+        child: Center(
+          child: ListView(
+            children: <Widget>[
+              Container(
+                height: 550,
+                decoration: new BoxDecoration(
+                  image: new DecorationImage(
+                    image: new AssetImage("lib/assets/starwars.jpg"),
+                    fit: BoxFit.fitWidth
+                  ), // DecorationImage
+                ), // BoxDecoration
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Container(
+                            height: 50,
+                            width: 50,
+                              child: Image(
+                                image: AssetImage("lib/assets/netflix.png")
+                              ), // Image
+                          ), // Container
+                          FlatButton(
+                            child: Text('Series', style: topMenuStyle),
+                          ), // FlatButton
+                          FlatButton(
+                            child: Text('Films', style: topMenuStyle),
+                          ), // FlatButton
+                          FlatButton(
+                            child: Text('My List', style: topMenuStyle),
+                          ), // FlatButton
+                        ], // Widget[]
+                      ), // Row
+                    ), // Container
+                  ], // Widget[]
+                ), // Column
+              ), // Container
+              Container(
+                padding: EdgeInsets.only(top: 20, bottom: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    FlatButton(
+                      child: Column(
+                        children: <Widget>[
+                          Icon(Icons.add, color: Colors.white, size: 30),
+                          Text("My List", style: buttonInfoStyle)
+                        ], // <Widget>[]
+                      ), // Column
+                      onPressed: () {
 
-                    },
-                  ), // FlatButton
-                  FlatButton(
-                    color: Colors.white,
-                    child: Row(
-                      children: <Widget>[
-                        Icon(Icons.play_arrow, color: Colors.black),
-                        Text("Play", style: buttonBlackStyle)
-                      ], // <Widget>[]
-                    ), // Row
-                    onPressed: () {
+                      },
+                    ), // FlatButton
+                    FlatButton(
+                      color: Colors.white,
+                      child: Row(
+                        children: <Widget>[
+                          Icon(Icons.play_arrow, color: Colors.black),
+                          Text("Play", style: TextStyle(color: Colors.black))
+                        ], // <Widget>[]
+                      ), // Row
+                      onPressed: () {
 
-                    },
-                  ), // FlatButton
-                  FlatButton(
-                    child: Column(
-                      children: <Widget>[
-                        Icon(Icons.info, color: Colors.white, size: 30),
-                        Text("Info", style: buttonInfoStyle)
-                      ], // <Widget>[]
-                    ), // Column
-                    onPressed: () {
+                      },
+                    ), // FlatButton
+                    FlatButton(
+                      child: Column(
+                        children: <Widget>[
+                          Icon(Icons.info, color: Colors.white, size: 30),
+                          Text("Info", style: buttonInfoStyle)
+                        ], // <Widget>[]
+                      ), // Column
+                      onPressed: () {
 
-                    },
-                  ), // FlatButton
-                ], // Widget[]
-              ), // Row
-            ), // Center
-            makePopularWidget("Popular on Netflix"),
-            makePopularWidget("Trending Now"),
-            makeContinueWatchingWidget("Continue Watching for Oliver"),
-          ], // Widget[]
-        ), // ListView
-      ), // Center
-    ); // Container
+                      },
+                    ), // FlatButton
+                  ], // Widget[]
+                ), // Row
+              ), // Center
+              makePopularWidget("Popular on Netflix"),
+              makePopularWidget("Trending Now"),
+              makeContinueWatchingWidget("Continue Watching for Oliver"),
+            ], // Widget[]
+          ), // ListView
+        ), // Center
+      ), // Container
+    ); // Material
   }
 
   Widget makePopularWidget(String title) {
